0ac125f483a4601fbeea05ff12f1463944e7bd30
Andrew Hany <andrewhany260@gmail.com>
2021-07-06 19:21:41 +0200

    pets models and router

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,18 @@
 require("dotenv").config();
 const db = require("./helpers/dbConnection");
+const petRouter = require("./routes/petRouter");
 db.connectDB();
+
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api/pets", petRouter);
+app.use((err, req, res, next) => {
+  res.status(500).json({ msg: err });
+});
+
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+  console.log(`Example app listening at http://localhost:${PORT}`);
+});
