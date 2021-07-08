6d554c5bb845adba51076f654ad1b0c5c5997487
Marina-Nady <rinanady92@gmail.com>
2021-07-08 14:20:56 +0200

    about

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,15 @@
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import PetsGallery from "./pages/PetsGallery/PetsGallery";
+import About from "./pages/About/About";
 
 function App() {
   return (
     <Router>
       <Switch>
         <Route path="/pets" component={PetsGallery}></Route>
+        <Route path="/about" component={About}></Route>
+
       </Switch>
     </Router>
   );
