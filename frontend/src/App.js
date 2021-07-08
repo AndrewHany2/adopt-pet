fa07a724cf93e415ea4f1a81c5ef254a9a7abeb8
Mahmoud Ibrahim <mahmoudm.ibrahim54@gmail.com>
2021-07-08 14:59:27 +0200

    Mahmoud First Commit Frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,21 @@ import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import PetsGallery from "./pages/PetsGallery/PetsGallery";
 import PetsInfo from "./pages/PetsInfo/PetsInfo";
+import NavBar from "./components/NavBar/NavBar"
+import SignIn from "./components/SignIn/SignIn";
+import SignUp from "./components/SignUp/SignUp";
+
 
 function App() {
   return (
     <Router>
+      <NavBar></NavBar>
       <Switch>
         <Route path="/pets/:id" component={PetsInfo}></Route>
         <Route path="/pets" component={PetsGallery}></Route>
+        <Route path="/signin" component={SignIn}></Route>
+        <Route path="/signup" component={SignUp}></Route>
+
       </Switch>
     </Router>
   );
