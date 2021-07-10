460081c7f4081b97af274d0ebb58098d0d9f1284
Mahmoudibrahim54 <60746387+Mahmoudibrahim54@users.noreply.github.com>
2021-07-10 23:30:19 +0200

    Mahmoud hamid (#7)
    * Mahmoud Hamid Frontend
    
    * add SignUp & SignIn to Pages
    
    * Remove Errors Of className & htmlFor
    
    * user model and router
    
    * Autherization and Authentication Backend
    
    * autherization and authentecation push and pull fixes
    
    * minor changes on navBar

---
+++ backend/helpers/Token.js
@@ -0,0 +1,18 @@
+const jwt = require('jsonwebtoken')
+const util = require('util')
+const signTokenPromiseBased = util.promisify(jwt.sign)
+const verifyTokenPromiseBased = util.promisify(jwt.verify)
+const secret = process.env.SECRET||"MEARNSTACK";
+
+const generateToken=(id)=>{
+    return signTokenPromiseBased({id:id},secret)
+}
+
+const verifyToken = (token)=>{
+    return verifyTokenPromiseBased(token,secret)
+}
+
+module.exports = {
+    generateToken,
+    verifyToken
+}
--- backend/routes/UserRouter.js
+++ backend/routes/UserRouter.js
@@ -1,9 +1,40 @@
 const userRouter = require("express").Router();
 const User = require("../models/UserModel");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+const { generateToken } = require("../helpers/Token");
+const verifyUser = require("../middlewares/VerifyUser");
 
-userRouter.post("/", async (req, res) => {
+userRouter.post("/login", async (req, res, next) => {
   const { body } = req;
   try {
+    if (body.email && body.password) {
+      const user = await User.findOne({ email: body.email });
+      if (user) {
+        const match = await bcrypt.compare(body.password, user.password);
+        if (match) {
+          const token = await generateToken(user._id);
+          res.status(200).json({ token });
+        } else {
+          res.status(400).json({ status: "password invalid" });
+        }
+      } else {
+        res.status(404).json({ status: "user invalid" });
+      }
+    }
+  } catch (error) {
+    next(new Error("server error"));
+  }
+});
+
+userRouter.post("/register", async (req, res) => {
+  const { body } = req;
+  try {
+    const salt = await bcrypt.genSalt(saltRounds);
+    const hashedPassword = await bcrypt.hash(body.password, salt);
+    body.password = hashedPassword;
+
+
     const user = new User({
       firstName: body.firstName,
       lastName: body.lastName,
@@ -17,17 +48,28 @@ userRouter.post("/", async (req, res) => {
     res.status(400).json(err);
   }
 });
-userRouter.get("/:id", async (req, res) => {
-  const { id } = req.params;
 
-  const user = await User.findOne({ _id: id });
-  res.status(200).json(user);
+
+userRouter.delete("/delete/:id", verifyUser, async (req, res) => {
+  try {
+    if (req.verified) {
+      const user = await User.deleteOne({ _id: req.verified });
+      res.status(200).json(user);
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
-
-userRouter.delete("/:id", async (req, res) => {
-  const { id } = req.params;
-  const user = await User.deleteOne({ _id: id });
-  res.status(200).json(user);
+userRouter.get("/:id",verifyUser, async (req, res) => {
+  try {
+    console.log(req.params.id)
+    if (req.verified) {
+      const user = await User.findOne({ _id: req.verified });
+      res.status(200).json(user);
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
 userRouter.get("/", async ({ query }, res) => {
