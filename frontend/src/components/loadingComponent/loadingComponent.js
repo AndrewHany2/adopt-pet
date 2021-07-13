a2db6fd3723f94284eaa7f15b10e40b913e6fefe
mohamedmagdy007 <mohamed.mo9751@gmail.com>
2021-07-13 22:52:03 +0200

    loadingComponent

---
+++ frontend/src/components/loadingComponent/loadingComponent.js
@@ -0,0 +1,11 @@
+import React from "react"
+import "./loadingComponent.css"
+ function LoadingComponent(){
+        
+    return(
+        <>
+       <span class="fas fa-paw loadingComponent"></span>
+        </>
+    )
+ }
+ export default LoadingComponent
