7bd503749b1db1cd3f5c1042d751e5455a1d30d0
Mahmoud Ibrahim <mahmoudm.ibrahim54@gmail.com>
2021-07-13 22:35:36 +0200

    NavBar Edits and Validation to SignUp and Sign In Added

--- frontend/src/pages/SignUp/SignUp.js
+++ frontend/src/pages/SignUp/SignUp.js
@@ -6,8 +6,118 @@ import "./SignUp.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFacebook } from "@fortawesome/free-brands-svg-icons";
 import { faGoogle } from "@fortawesome/free-brands-svg-icons";
+import { useState } from "react";
+import Joi from "joi";
 
 function SignUp() {
+  const [userInfo, setUserInfo] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    country: "",
+    city: "",
+    phone: "",
+    passwordObj: { password: "", repeatPassword: "" },
+  });
+  const [errors, setErrors] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    country: "",
+    city: "",
+    phone: "",
+    password: "",
+    repeatPassword: "",
+  });
+
+  const schema = {
+    email: Joi.string()
+      .email({
+        minDomainSegments: 2,
+        tlds: false,
+      })
+      .required(),
+    firstName: Joi.string().alphanum().min(3).max(30).required(),
+    lastName: Joi.string().alphanum().min(3).max(30).required(),
+    phone: Joi.string()
+      .length(11)
+      .pattern(/^(010|012|011|015)[0-9]{8}$/)
+      .required(),
+  };
+
+  const passwordSchema = Joi.object({
+    password: Joi.string()
+      .min(8).max(30)
+      .pattern(
+        new RegExp(
+          "^(((?=.*[a-z])(?=.*[A-Z]))|((?=.*[a-z])(?=.*[0-9]))|((?=.*[A-Z])(?=.*[0-9])))(?=.{8,})"
+        )
+      )
+      .messages({
+        "any.only": `password didn't match`,
+        "string.base": `password should contain Capital, Small letters and Special Char`,
+        "string.min": `should have a minimum length of {#limit}`,
+        "string.pattern.base": `Password Must Contain Captial and Small Letters`,
+        "string.max": `should have a maximum length of {#limit}`,
+      }),
+    repeatPassword: Joi.any().equal(Joi.ref("password")).messages({
+      "any.only": `password didn't match`,
+    }),
+  //  password: Joi.string()
+  //   .min(8).max(30)
+  //   .pattern(
+  //     new RegExp(
+  //       "^(((?=.*[a-z])(?=.*[A-Z]))|((?=.*[a-z])(?=.*[0-9]))|((?=.*[A-Z])(?=.*[0-9])))(?=.{8,})"
+  //     )
+  //   ).error(errors => {
+  //     errors.forEach(err => {
+  //       switch (err.code) {
+  //         case "any.only":
+  //           err.message = "value not allowed";
+  //           break;
+  //         case "string.min":
+  //           err.message = `Value should have at least ${err.local.limit} characters!`;
+  //           break;
+  //           case "string.max":
+  //           err.message = `Value should have at most ${err.local.limit} characters!`;
+  //           break;
+  //         case "string.pattern.base":
+  //           err.message = `Password Must Contain Captial, Small Letters and Special Characters`;
+  //           break;
+  //         default:
+  //           break;
+  //       }
+  //     });
+  //     return errors;
+  //   }),
+  });
+
+  const validatePassword = (e) => {
+    let value = e.target.id;
+
+    let myData = { ...userInfo };
+    myData.passwordObj[value] = e.target.value;
+    setUserInfo(myData);
+    const validate = passwordSchema.validate(myData.passwordObj);
+    let myErrors = { ...errors };
+    myErrors["password"] = validate.error
+      ? validate.error.details[0].message
+      : null;
+    setErrors(myErrors);
+    console.log(validate);
+  };
+  const validateFields = (e) => {
+    let value = e.target.id;
+
+    let myData = { ...userInfo };
+    myData[value] = e.target.value;
+    setUserInfo(myData);
+    const validate = schema[value].validate(e.target.value);
+    let myErrors = { ...errors };
+    myErrors[value] = validate.error ? validate.error.details[0].message : null;
+    setErrors(myErrors);
+  };
+
   return (
     <>
       <div className="container-fluid bg-img">
@@ -16,54 +126,118 @@ function SignUp() {
             <form>
               <div className="form-row mt-4 mr-3 ml-3 pt-5 pb-2">
                 <div className="col-md-6 mb-3">
-                  <label htmlFor="validationCustom01">First name</label>
+                  <label htmlFor="firstName">First name</label>
                   <input
                     type="text"
                     className="form-control theme-border"
-                    id="validationCustom01"
+                    id="firstName"
                     required
+                    value={userInfo.firstName}
+                    onChange={validateFields}
                   />
                 </div>
+
                 <div className="col-md-6 mb-3">
-                  <label htmlFor="validationCustom02">Last name</label>
+                  <label htmlFor="lastName">Last name</label>
                   <input
                     type="text"
                     className="form-control theme-border"
-                    id="validationCustom02"
+                    id="lastName"
                     required
+                    value={userInfo.lastName}
+                    onChange={validateFields}
                   />
                 </div>
+                {errors.firstName && (
+                  <div className="alert alert-danger">{errors.firstName}</div>
+                )}
+                {errors.lastName && (
+                  <div className="alert alert-danger">{errors.lastName}</div>
+                )}
               </div>
               <div className="form-row mr-3 ml-3 pb-2">
                 <div className="col-12 mb-3">
-                  <label htmlFor="validationCustom01">Email</label>
+                  <label htmlFor="email">Email</label>
                   <input
                     type="email"
                     className="form-control theme-border"
-                    id="login-email"
+                    id="email"
                     placeholder="email@example.com"
+                    value={userInfo.email}
+                    onChange={validateFields}
                   />
                 </div>
               </div>
-              <div className="form-row mr-3 ml-3">
-                <div className="col-md-6 mb-3">
-                  <label htmlFor="validationCustom01">City</label>
-                  <input
-                    type="text"
-                    className="form-control theme-border"
-                    id="validationCustom01"
-                    required
-                  />
-                </div>
-                <div className="col-md-6 mb-3">
-                  <label htmlFor="validationCustom02">Phone</label>
+              {errors.email && (
+                <div className="alert alert-danger">{errors.email}</div>
+              )}
+              <div className="form-row mr-3 ml-3 pb-2">
+                <div className="col-12 mb-3">
+                  <label htmlFor="phone">Phone</label>
                   <input
                     type="phone"
                     className="form-control theme-border"
-                    id="validationCustom02"
+                    id="phone"
+                    value={userInfo.phone}
+                    onChange={validateFields}
+                  />
+                </div>
+                {errors.phone && (
+                  <div className="alert alert-danger">{errors.phone}</div>
+                )}
+              </div>
+              <div className="form-row mr-3 ml-3 pb-2">
+                <div className="col-md-6 mb-3">
+                  <label htmlFor="country">Country</label>
+                  <input
+                    type="text"
+                    className="form-control theme-border"
+                    id="country"
                     required
                   />
                 </div>
+                <div className="col-md-6 mb-3">
+                  <label htmlFor="city">City</label>
+                  <input
+                    type="text"
+                    className="form-control theme-border"
+                    id="city"
+                    required
+                  />
+                </div>
+              </div>
+
+              <div className="form-row mr-3 ml-3">
+                <div className="col-md-6 mb-3">
+                  <label htmlFor="password">Password</label>
+                  <input
+                    type="password"
+                    className="form-control theme-border"
+                    id="password"
+                    required
+                    value={userInfo.password}
+                    onChange={validatePassword}
+                  />
+                </div>
+                <div className="col-md-6 mb-3">
+                  <label htmlFor="repeatPassword">Confirm Password</label>
+                  <input
+                    type="password"
+                    className="form-control theme-border"
+                    id="repeatPassword"
+                    required
+                    value={userInfo.repeatPassword}
+                    onChange={validatePassword}
+                  />
+                </div>
+                {errors.password && (
+                  <div className="alert alert-danger">{errors.password}</div>
+                )}
+                {errors.repeatPassword && (
+                  <div className="alert alert-danger">
+                    {errors.repeatPassword}
+                  </div>
+                )}
               </div>
               <div className="form-group ml-4">
                 <div className="form-check">
@@ -103,7 +277,7 @@ function SignUp() {
             <div className="dropdown-divider p-1 "></div>
             <p className="text-inverse text-center">
               Already have an account?{" "}
-              <Link to="<?= base_url() ?>auth/login" data-abc="true">
+              <Link to="/signin" data-abc="true">
                 Login
               </Link>
             </p>
@@ -117,7 +291,6 @@ function SignUp() {
               </Link>
             </p>
           </div>
-
         </div>
       </div>
     </>
