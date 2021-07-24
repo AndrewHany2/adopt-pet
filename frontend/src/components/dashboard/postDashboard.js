46fdaec30172b34ecba2fcf1501206be67f16de4
mohamedmagdy007 <mohamed.mo9751@gmail.com>
2021-07-24 21:41:21 +0200

    postDashboard

--- frontend/src/components/dashboard/postDashboard.js
+++ frontend/src/components/dashboard/postDashboard.js
@@ -1,36 +1,26 @@
 import React from "react"
-
-function PostDashboard(){
+import { useEffect, useState } from "react";
+function PostDashboard(props){
+    const [user, setUser] = useState({});
+  useEffect(() => {
+    fetch(`/api/user/${props.pets.owner}`)
+    .then(response => response.json())
+    .then(data => setUser(data))
+  },[]);
     return(
-<div className="table-responsive">
-   <table class="table">
-    <thead>
+<tbody>
         <tr>
-        <th scope="col">User</th>
-        <th scope="col">Name-pet</th>
-        <th scope="col">Gender</th>
-        <th scope="col">Type</th>
-        <th scope="col">DateOfBirth</th>
-        <th scope="col">Size</th>
-        <th scope="col">image</th>
-        <th colSpan="true" className="text-center">status</th>
-        </tr>
-    </thead>
-    <tbody>
-        <tr>
-        <th scope="row">Email@gmail.com</th>
-        <td>Fluffy</td>
-        <td>Female</td>
-        <td>dog</td>
-        <td>7-7-2020</td>
-        <td>Medium</td>
-        <td><img src="../resources/adoption1-185x185.jpg" width="50" className="rounded-circle"/></td>
+        <th scope="row">{user.email}</th>
+        <td>{props.pets.name}</td>
+        <td>{props.pets.gender}</td>
+        <td>{props.pets.petType}</td>
+        <td>{props.pets.vaccinated}</td>
+        <td>{props.pets.size}</td>
+        <td><img src={props.pets.image} width="50" className="rounded-circle"/></td>
         <td><button className="btn btn-success">Accept</button></td>
         <td><button className="btn btn-danger">Reject</button></td>
         </tr>
     </tbody>
-    </table>
-</div>
     ) 
 
 }
--- frontend/src/pages/Dashboard/Dashboard.js
+++ frontend/src/pages/Dashboard/Dashboard.js
@@ -1,15 +1,43 @@
 import React from "react";
 import PostDashboard from "../../components/dashboard/postDashboard";
 import AdoptRequest from "../../components/dashboard/adoptRequest";
+import { useEffect, useState } from "react";
 export default function Dashboard() {
+  const [pets, setPets] = useState([{}]);
+  useEffect(() => {
+    fetch(`/api/pets/`)
+    .then(response => response.json())
+    .then(data => setPets(data))
+  },[]);
+  console.log(pets)
+  
   return (
    <>
    <div className="container">
       <div className="row mb-5 mt-3">    
         <div className="col-12 col-md-6">
         <h2>Post</h2>
-      <PostDashboard />
-        </div>
+      <div className="table-responsive">
+      <table class="table">
+        <thead>
+            <tr>
+            <th scope="col">User</th>
+            <th scope="col">Name-pet</th>
+            <th scope="col">Gender</th>
+            <th scope="col">Type</th>
+            <th scope="col">Vaccinated</th>
+            <th scope="col">Size</th>
+            <th scope="col">image</th>
+            <th colSpan="true" className="text-center">status</th>
+            </tr>
+        </thead>
+            {pets?.list && pets.list.map((pet)=>{
+                  return <PostDashboard key={pet._id} pets={pet} />
+                })}
+            </table>
+      </div> 
+            </div>
+
         <div className="col-12 col-md-6">
         <h2>AdoptionRequest</h2>
         <AdoptRequest/>
