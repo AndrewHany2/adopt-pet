7bca3353dda28b9c3262a728d5233fafe6be77df
Marina-Nady <rinanady92@gmail.com>
2021-07-25 23:49:00 +0200

    edit2

--- backend/models/UserModel.js
+++ backend/models/UserModel.js
@@ -25,7 +25,7 @@ const userSchema = mongoose.Schema({
   },
   role: {
     type: String,
-    enum: ["USER", "ADMIN", "SUPWE_ADMIN"],
+    enum: ["USER", "ADMIN", "SUPER_ADMIN"],
     default: "USER",
   },
   postedPets: {
--- frontend/src/components/dashboard/adoptRequest.js
+++ frontend/src/components/dashboard/adoptRequest.js
@@ -1,23 +1,8 @@
 import React from "react";
 import axios from "axios";
-import moment from 'moment'
+import moment from 'moment';
 
 function AdoptRequest(props) {
-  const [user, setUser] = useState({});
-  const [pet, setPet] = useState({});
-   const requestedUsers = ()=>{
-      fetch(`/api/user/${props.requests.requestedUserId}`)
-      .then((res)=> res.json())
-      .then(data => setUser(data))
-      .catch(err => console.log("Error"));
-  }
-
-  const requestedPets = ()=>{
-      fetch(`/api/pets/${props.requests.petId}`)
-      .then(res => res.json())
-      .then(data => setPet(data))
-      .catch(err => console.log("Error"));
-  }
   const handleAccept = () => {
     axios.patch(`/api/admin/application/accept/${props.request._id}`);
     props.handleClick(props.request._id, "requests");
@@ -28,10 +13,7 @@ function AdoptRequest(props) {
     props.handleClick(props.request._id, "requests");
   };
 
-  useEffect(() => {
-    requestedUsers();
-    requestedPets();
-  },[]);
+
 
   return (
     <tbody>
@@ -39,7 +21,7 @@ function AdoptRequest(props) {
         <th scope="row">{props.request.requestedUserId.email}</th>
         <td>{props.request.petId.name}</td>
         <td>{props.request.petId.gender}</td>
-        <td>{props.request.petId.dateOfBirth}</td>
+        <td>{moment(props.request.petId.dateOfBirth).format('DD/MM/YYYY')}</td>
         <td>{props.request.petId.petType}</td>
         <td>{props.request.petId.size}</td>
         <td>
