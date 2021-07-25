e230c350303c63ad8ddb7343b4d63a4b3d1543bf
Mohammed Al-Daragily <mohammed.aldaragily@gmail.com>
2021-07-25 23:05:19 +0200

    More Info Fix

--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -1,20 +1,33 @@
-import AboutUs from "../components/AboutUs"
-import Counter from "../components/Counter"
-import HomeCarouel from "../components/homeCarousel"
-import Slider from "../components/Slider"
-import ContactUs from "./ContactUs/contactUs"
+import AboutUs from "../components/AboutUs";
+import Counter from "../components/Counter";
+import HomeCarouel from "../components/homeCarousel";
+import Slider from "../components/Slider";
+import ContactUs from "./ContactUs/contactUs";
+import PetsCards from "../components/PetsCards";
+import { useEffect } from "react";
+import { getPets } from "../store/actions/petActions";
+import { useDispatch, useSelector } from "react-redux";
 
 export default function Home() {
-
+    const dispatch = useDispatch();
+    const pets = useSelector((state) => state.pets);
+    useEffect(() => {
+        dispatch(getPets("", "", "", "", 4));
+    }, []);
     return (
         <>
-        <HomeCarouel />
-        <div className="container mt-4">
-            <AboutUs />
-            <Counter />
-            <Slider />
-            <ContactUs />
-        </div>
+            <HomeCarouel />
+            <div className="container mt-4">
+                {pets.info ? (
+                    <div className="mt-5">
+                        <PetsCards pets={pets} />{" "}
+                    </div>
+                ) : null}
+                <AboutUs />
+                <Counter />
+                <Slider />
+                <ContactUs />
+            </div>
         </>
-    )
+    );
 }
